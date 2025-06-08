7532c67eb69e0fdd69b0852d76b844b37be60550
Vedant2802 <avedant981@gmail.com>
2025-06-09 00:13:32 +0530

    some playing around

--- DomManipulation/index.js
+++ DomManipulation/index.js
@@ -4,18 +4,11 @@
     2.Add some attributes 
     3.Append Element to DOM
     4.Remove HTML Element
+    5.Modifying Content
+    6.Read , write , remove attributes
+    7.Traversing / Navigating DOM
+    8.Controlling Visibilities
+    9.Manupulating Classes
 */
 
-const newH1 = document.createElement("h1");
-newH1.textContent = "Akshat";
-newH1.id = "myname";
-// newH1.setAttribute("id", "myname");
-
-// document.body.append(newH1);
-const box1 = document.getElementById("box2");
-// box1.nextElementSibling.append(newH1);
-// document.body.prepend(newH1);
-
-//if you want to insert your new element between the two different elements
-
-document.body.insertBefore(newH1, box1);
+document.getElementById("items");
